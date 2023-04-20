d098af8f60380ba1c2450de4276461b236d43424
Renata Macedo <renata.macedo@etec.sp.gov.br>
2023-04-20 18:05:58 -0300

    estilização do login

--- resources/views/usuarios/login.blade.php
+++ resources/views/usuarios/login.blade.php
@@ -4,59 +4,32 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-        <title>Tela de Login</title>
-        <style>
-            body{
-                font-family: Arial, Helvetica, sans-serif;
-                background-color: beige;
-            }
-            div{
-                background-color: rgba(0,0,0, 0.8);
-                position: absolute;
-                top: 50%;
-                left: 50%;
-                transform: translate(-50%, -50%);
-                padding: 80px;
-                border-radius: 15px;
-                color: #fff;
-            }
-            input{
-                padding: 15px;
-                border: none;
-                outline: none;
-                font-size: 15px;
-            }
-            button{
-                background-color: dodgerblue;
-                border: none;
-                padding: 15px;
-                width: 100%;
-                border-radius: 10px;
-                color: white;
-                font-size: 15px;
-            }
-            button:hover{
-                background-color: deepskyblue;
-                cursor: pointer;
-            }
-        </style>
+        @vite([
+            'resources/sass/app.scss'
+        ])
+
+        <title>Login</title>
 
     </head>
     <body>
-       <div>
-        <form action="{{ route('login.store') }}" method="POST">
-            @csrf
+        <section class="area-login">
+            <div class="login">
+                <div>
+                    <img src="image/logo.jpg">
+                </div>
+
+                <form action="{{ route('login.store') }}" method="POST">
+                    @csrf
 
             <h1>Login</h1>
-            <input type="text" name="email" placeholder="Email, usuario, ou cnpj">
+            <input type="text" name="email" placeholder="Email">
 
             @error('email')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
 
-            <br><br>
+            <br>
             <input type="password" name="senha" placeholder="Senha">
 
             @error('senha')
@@ -73,7 +46,9 @@
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
 
-            <button>Enviar</button>
+            <input type="submit" value="entrar">
+            </div>
+        </section>
         </form>
        </div>
     </body>
