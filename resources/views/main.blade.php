bdec151e28d1742a0084c61d8c180e16be8faf46
RenanGSilvaa <profissionalrenann@outlook.com>
2023-05-17 22:33:34 -0300

    Views usando extends

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,24 +1,36 @@
 <!DOCTYPE html>
-    <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-        <title>OI</title>
-    </head>
-    <body>
-        <div>
-            <head>
-                <a href="{{route('login')}}">Login</a>
-                <a href="{{route('usuarios.create')}}">Cadastrar-se</a>
-                {{-- <a href="{{route('opinioes.create')}}">Fazer uma publicação</a> --}}
-                <a href="{{route('empresas.create')}}">Cadastrar uma empresa</a>
-                <a href="{{ route('login.destroy') }}">Sair</a>
-            </head>
+<html lang="pt-br">
+    @yield('head')
+<body>
+    <!-- Navbar -->
+    <nav>
+        <div class="logo">
+            <a class="main-text" href="#home">
+                <h1 class="img-logo">OpineCom.</h1>
+            </a>
         </div>
-
-
+        <input type="checkbox" id="checkbox">
+        <label for="checkbox" id="icon">
+            <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+        </label>
+        <ul>
+            <li><a href="{{route('opinioes.create')}}" class="links">Opinar!</a></li>
+            <li><a href="{{route('login')}}" class="links">Login</a></li>
+            <li><a href="{{route('empresas.create')}}" class="links">Cadastro de empresa</a></li>
+            <li><a href="{{route('usuarios.create')}}" class="links">Cadastro de usuário</a></li>
+            {{-- <li><a href="{{route('opinioes.create')}}" class="links">Opinião</a></li> --}}
+            <li><a href="#" class="links">Contato</a></li>
+            <li><a href="{{route('login.destroy')}}" class="links">Sair</a></li>
+        </ul>
+    </nav>
         @yield('content')
+    <!-- Footer -->
+    <footer>
+        <div class="logo">
+            <p class="copyright-footer">©2023 OpineCom </p>
+        </div>
+    </footer>
+
+    <script src="/js/script.js" defer></script>
     </body>
 </html>
--- resources/views/usuarios/create.blade.php
+++ resources/views/usuarios/create.blade.php
@@ -1,16 +1,22 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, inicial-scale=1.0">
+@extends('main')
 
-        @vite([
-            'resources/sass/app.scss'
-        ])
+@section('head')
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, inicial-scale=1.0">
 
-        <title>Formulário de Cadastro</title>
-    </head>
+    @vite([
+        'resources/sass/create.scss'
+    ])
+
+    <title>Formulário de Cadastro</title>
+</head>
+
+
+@section('content')
+
+
 
     <body>
         <div class="container">
@@ -79,5 +85,4 @@
             </div>
         </div>
     </body>
-
-</html>
+@endsection
