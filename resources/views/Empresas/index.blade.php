e4b7fba8c7cebf703373bcfe33bd49e9d82fae78
Renata Macedo <renata.macedo@etec.sp.gov.br>
2023-05-19 22:32:23 -0300

    estilização index e view

--- resources/views/Empresas/index.blade.php
+++ resources/views/Empresas/index.blade.php
@@ -5,17 +5,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+        @vite(['resources/sass/index.scss'])
         <title>Lista de Empresas</title>
     </head>
     <body>
         <div class="container">
             <h1>Lista de Empresas</h1>
 
-            <a class="btn btn-outline-success my-2" href="{{ route('empresas.create') }}">Nova Empresa</a>
+            <button class="btn btn my-2" href="{{ route('empresas.create') }}">Nova Empresa</button>
 
-            <table class="table table-hover table-bordered table-primary">
-                <tr class="table-dark">
+            <table class="table table table-primary">
+                <tr class="barra">
                     <th>Razão Social</th>
                     <th>CNPJ</th>
                     <th>Endereço</th>
